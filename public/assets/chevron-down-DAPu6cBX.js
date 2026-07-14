37515319dcf24159a7226489987aa0df5885536f
S U F Y A N <thesufyanansari1@gmail.com>
2026-07-14 14:37:08 +0300

    chore: remove root public folder and untrack generated assets

--- public/assets/chevron-down-DAPu6cBX.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./index-Di12RWir.js";var t=e(`chevron-down`,[[`path`,{d:`m6 9 6 6 6-6`,key:`qrunsl`}]]);export{t};
--- public/assets/plus-fI0uvRsA.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./index-Di12RWir.js";var t=e(`plus`,[[`path`,{d:`M5 12h14`,key:`1ays0h`}],[`path`,{d:`M12 5v14`,key:`s699le`}]]);export{t};
